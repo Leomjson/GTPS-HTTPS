167a174d6be97ced41ab72ead4f7e948b8dd3d98
leo <89754898+Leomjson@users.noreply.github.com>
2023-01-21 23:46:04 -0600

    Size Optimization

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
 /*
-    HTTPS with connection control
-	!! ports are still vulnerable !!
-
-	~ Comments, code was made by LeomJson (leo)
-	HTTPS Credibility: https://github.com/yhirose/cpp-httplib
+	HTTPS with connection control
+   !! ports are still vulnerable !!
+   ~ Comments, code was made by LeomJson (leo)
+   HTTPS Credibility: https://github.com/yhirose/cpp-httplib
 */
-#include "httplib.h"
+#include "httplib.hpp"
 #include "json.hpp"
 #include <future>
 #include <filesystem>
 httplib::SSLServer server("cert.pem", "key.pem");
 using namespace std;
-using req = httplib::Request;
-using res = httplib::Response;
 vector<thread> backtasks;
 
-// This prevents DDoS- sense your rejecting "disrup", but as commented above this only prevents mild disruptions and WONT protect firewall ports.
-#define r_seconds 4 // second(s)
-#define requests 3 // allowing "requests" per "seconds" else reject connection.
+#define r_seconds 4  
+#define requests 3        
 
 const char* server_data =
 R"(server|127.0.0.1
@@ -26,19 +22,14 @@ type|1
 meta|cmeta
 RTENDMARKERBS1001)";
 const char* gov = 
-R"(<style>h1 {
-text-align: center;
-} 
-</style> 
+R"(<style>h1 { text-align: center; }
+</style>
 <body>
 <h1><b><big>Unauthorized Access</big></b></h1>
 </body>)";
-class connection
-{
-public:
+class connection { public:
 	string ip = "";
 	int attempts = 0;
-	bool timeout = false;
 };
 map<string, connection> connection_data;
 void SaveConnectionData(connection data, string ip) {
@@ -46,7 +37,6 @@ void SaveConnectionData(connection data, string ip) {
 	nlohmann::json j; j.dump(1);
 	j["ip"] = data.ip;
 	j["attempts"] = data.attempts;
-	j["timeout"] = data.timeout;
 	w << setw(2) << j;
 }
 connection LoadConnectionData(string ip) {
@@ -55,26 +45,24 @@ connection LoadConnectionData(string ip) {
 	connection data;
 	data.ip = j["ip"];
 	data.attempts = j["attempts"];
-	data.timeout = j["timeout"];
-	return data; // return the data for using the connection object elsewhere
+	return data;
 }
 void append_reset(string ip) {
 	while (true) {
-		for (auto it = connection_data.begin(); it != connection_data.end(); ++it) {
-			if (it->first == ip) {
-				if (it->second.timeout == true) {
-					this_thread::sleep_for(chrono::seconds(r_seconds));
-					it->second.timeout = false, it->second.attempts -= requests;
-					connection data = LoadConnectionData(ip);
-					data.timeout = it->second.timeout, data.attempts = it->second.attempts;
-					SaveConnectionData(data, it->second.ip);
-				}
+		for (auto it = connection_data.begin(); it != connection_data.end(); ++it) if (it->first == ip)
+		{
+			if (it->second.attempts > requests) {
+				this_thread::sleep_for(chrono::seconds(r_seconds));
+				it->second.attempts -= requests;
+				connection data = LoadConnectionData(ip);
+				data.attempts = it->second.attempts;
+				SaveConnectionData(data, it->second.ip);
 			}
 		}
 		this_thread::sleep_for(1ms);
 	}
 }
-bool request(const req req, res res)
+bool request(const httplib::Request req)
 {
 	ifstream r("connection/" + req.remote_addr);
 	if (not r.is_open()) {
@@ -84,34 +72,28 @@ bool request(const req req, res res)
 		SaveConnectionData(new_data, req.remote_addr);
 		connection_data.emplace(req.remote_addr, new_data);
 	}
-	else {
-		for (auto it = connection_data.begin(); it != connection_data.end(); ++it) {
-			if (it->first == req.remote_addr) {
-				it->second.attempts++;
-				if (it->second.attempts > requests) {
-					it->second.timeout = true;
-					backtasks.emplace_back(append_reset, req.remote_addr);
-					return false;
-				}
-				connection data = LoadConnectionData(req.remote_addr);
-				data.attempts = it->second.attempts;
-				data.timeout = it->second.timeout;
-				SaveConnectionData(data, it->second.ip);
-			}
+	else for (auto it = connection_data.begin(); it != connection_data.end(); ++it) if (it->first == req.remote_addr)
+	{
+		it->second.attempts++;
+		if (it->second.attempts > requests) {
+			backtasks.emplace_back(append_reset, req.remote_addr);
+			return false;
 		}
+		connection data = LoadConnectionData(req.remote_addr);
+		data.attempts = it->second.attempts;
+		SaveConnectionData(data, it->second.ip);
 	}
 	return true;
 }
 int main()
 {
-	for (const auto &i : filesystem::directory_iterator("./connection/")) {
-		if (not filesystem::is_directory(i.path())) {
-			connection data = LoadConnectionData(i.path().filename().string());
-			connection_data.emplace(i.path().filename().string(), data);
-		}
+	for (const auto& i : filesystem::directory_iterator("./connection/")) if (not filesystem::is_directory(i.path()))
+	{
+		connection data = LoadConnectionData(i.path().filename().string());
+		connection_data.emplace(i.path().filename().string(), data);
 	}
-	server.Post(("/growtopia/server_data.php"), [&](const req& req, res& res) {
-		future<bool> Request = async(request, req, res); Request.wait();
+	server.Post(("/growtopia/server_data.php"), [&](const httplib::Request& req, httplib::Response& res) {
+		future<bool> Request = async(request, req); Request.wait();
 	if (not Request.get()) {
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
 		cerr << req.remote_addr << " was rejected" << endl;
@@ -123,9 +105,8 @@ int main()
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 	res.set_content(server_data, "text/plain");
 		});
-	server.Get(("/growtopia/server_data.php"), [&](const req& req, res& res) { res.set_content(gov, "text/html"); });
-	server.Get(("/cache"), [&](const req& req, res& res) { res.set_content(gov, "text/html"); });
+	server.Get(("/growtopia/server_data.php"), [&](const httplib::Request& req, httplib::Response& res) { res.set_content(gov, "text/html"); });
+	server.Get(("/cache"), [&](const httplib::Request& req, httplib::Response& res) { res.set_content(gov, "text/html"); });
 	server.listen("0.0.0.0", 443);
-	while (server.is_running());
-	return 0; // program ends
+	return 0;
 }
